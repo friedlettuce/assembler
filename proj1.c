24a269c8551b1e477806e1649a68cba2103574ea
ga17 <ga17@my.fsu.edu>
2020-02-10 21:42:42 -0500

    Added to parser and test file

--- proj1.c
+++ proj1.c
@@ -1,22 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int parse(char** symbols);
 
-int main(int argc, char* argv[]){
-	if(argc < 2){ return -1; }
+int main(int argc, char * argv[]){
 
+	printf("!\n");
 	char** symbols = NULL;
 	int symbol_count = parse(symbols);
 
-	if(symbol_count == -1){ 
+	if(symbol_count < 1){ 
 		printf("Invalid symbols\n");
 		return -1;
 	}
+	
+	for(int i = 0; i < symbol_count; ++i)
+		printf("%s\n", symbols[i]);
 
+	free(symbols);
 	return 0;
 }
 
 int parse(char** symbols){
+
+	int symbol_count = 0;
+	char* str = NULL;
+
+	printf("AAA\n");
+	do{
+		scanf("%ms", &str);
+		printf("%s\n", str);
+		
+		if(symbol_count < 1){
+			symbols = (char**)malloc(sizeof(char*));
+		} else{
+			symbols = (char**)realloc(symbols, (symbol_count+1) * sizeof(char*));
+		}
+
+		symbols[symbol_count] = (char*)malloc((strlen(str)+1) * sizeof(char));
+		strcpy(symbols[symbol_count], str);
+		
+		++symbol_count;
+	}while('\n' != getchar());
 	
-	return -1;
+	return symbol_count;
 }
